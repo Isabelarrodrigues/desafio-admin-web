0d4636eeb6cd8f6628be4000f93e296d96bf99c4
Allan Kildare <allankildare27@gmail.com>
2021-03-20 18:12:17 -0300

    feat: funcao para requisicao tornou-se dinamica

--- src/components/Model/ReqUsers.js
+++ src/components/Model/ReqUsers.js
@@ -1,10 +1,10 @@
-export default async function ReqUsers() {
+export default async function ReqUsers(path) {
     const url =
     process.env.NODE_ENV === "production"
       ? "/api"
       : "http://localhost:3001/api"
 
-    const response = await fetch(`${url}/users`)
+    const response = await fetch(`${url}/${path}`)
     const json = await response.json()
     return json
 }
